89f37eca212ca5a0b17684cca26537827bd20021
Gilberto <43886382+gilbertrec@users.noreply.github.com>
2024-03-12 19:04:27 +0100

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -22,6 +22,16 @@ const data = [
       'In this paper, we expanded the current body of knowledge on data smells by proposing a two-step investigation into the matter. First, we updated an existing literature review in an effort of cataloguing the currently existing data smells and the tools to detect them. Afterward, we assess the prevalence of data smells and their correlation with data quality metrics. We identify a novel set composed of 12 data smells distributed across three additional categories.',
     link: '/personal_doc/publications/C2.pdf',
   },
+  {
+    "title": "When Code Smells Meet ML: On the Lifecycle of ML-specific Code Smells in ML-enabled Systems",
+    "subtitle": "Investigating the Emergence and Evolution of ML-specific Code Smells",
+    "authors": "G. Recupito, G. Giordano, F. Ferrucci, D. Di Nucci, F. Palomba",
+    "conference": "MSR 2024 - Registered Report Track",
+    "date": "2024-03-12",
+    "desc": "The paper investigates the emergence and evolution of ML-specific code smells, which are sub-optimal implementation solutions applied on ML pipelines that may decrease both the quality and maintainability of ML-enabled systems. The authors present a plan to study ML-specific code smells by empirically analyzing their prevalence in real ML-enabled systems, how they are introduced and removed, and their survivability. They propose conducting an exploratory study, mining a large dataset of ML-enabled systems, and analyzing over 400k commits about 337 projects. They plan to track and inspect the introduction and evolution of ML smells through CodeSmile, a novel ML smell detector built for this investigation.",
+    "link": "/personal_doc/publications/C3.pdf"
+},
+
 ];
 
 export default data;
