2a6784bb899e0d062488a45af891d649c2661af7
gilbertrec <43886382+gilbertrec@users.noreply.github.com>
2025-06-15 18:47:26 +0200

    updated for scholar indexing

--- src/components/Projects/Cell.js
+++ src/components/Projects/Cell.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Helmet } from 'react-helmet';
 const Cell = ({ data }) => (
   <div className="cell-container">
+     <Helmet>
+      <meta name="citation_title" content={data.title} />
+      <meta name="citation_author" content={data.authors} />
+      <meta name="citation_conference_title" content={data.conference} />
+      <meta name="citation_publication_date" content={data.date} />
+      <meta name="citation_pdf_url" content={data.link} />
+    </Helmet>
     <article className="mini-post">
       <header>
         <h3 className="title_published"><a href={data.link}>{data.title}</a></h3>
